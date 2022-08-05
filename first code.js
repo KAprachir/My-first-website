0004b742f9b52c2ec5e32e7eb84a39e9f3fddc20
KAprachir <110666388+KAprachir@users.noreply.github.com>
2022-08-06 00:50:02 +0600

    Add files via upload

---
+++ first code.js
@@ -0,0 +1,20 @@
+var petdog = "rex";
+var petcat = "papper";
+console.log(petdog,petcat);
+console.log("my pet dog's name is",+petdog);
+console.log("my pet cat's name is",+petcat);
+var catsound = "purr";
+var dogsound ="woof";
+console.log( petdog,"says",+dogsound);
+console.log( petcat,"says",+catsound);
+catsound ="meow";
+console.log(petcat, "says",+catsound);
+console.log(10>9 || 10<8);
+var mon = 1;
+var tue = 2;
+var wed = 1;
+var thu = 2;
+var fri = 3;
+console.log(mon + tue + wed + thu + fri); // 9
+
+
